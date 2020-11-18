33c590fe8265e8ea54fe51097c1bcc1da96dbd27
Josh <10923379@my.uvu.edu>
2020-11-18 15:52:21 -0700

    Built Personal Project primitives

---
+++ PersonalProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10f;
+    public GameObject projectilePrefab;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      horizontalInput = Input.GetAxis("Horizontal");
+
+      transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);  
+
+      if(transform.position.x < 4)
+      {
+          transform.position = new Vector3(4, transform.position.y, transform.position.z);
+      }
+      if(transform.position.x > 15)
+      {
+          transform.position = new Vector3(15, transform.position.y, transform.position.z);
+      }
+      if(Input.GetKeyDown(KeyCode.Space))
+      {
+          Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+      }
+    }
+}
