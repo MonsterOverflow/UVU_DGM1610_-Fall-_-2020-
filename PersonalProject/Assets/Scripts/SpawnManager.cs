5d6fd251b50e0fb60e9083d999f1e829de29942b
Josh <10923379@my.uvu.edu>
2020-12-11 00:26:10 -0700

    Added and Completed Lesson 5

--- PersonalProject/Assets/Scripts/SpawnManager.cs
+++ PersonalProject/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+   public GameObject[] EnemyPrefab;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomEnemy", 2, 1.5f);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnRandomEnemy()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+            Instantiate(EnemyPrefab[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
